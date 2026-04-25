e89b6eace4a97ad6f7c27fd5602dc8648b252c1a
amitbenyehuda24 <amitby11@gmail.com>
2026-04-25 21:49:18 +0000

    Completed Task 1: Hello World

---
+++ user/helloworld.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    // Print the required message to the screen
+    printf("Hello World xv6\n");
+    
+    // Exit the program safely
+    exit(0);
+}
